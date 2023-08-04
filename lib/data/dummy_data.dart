6f916ab14023732504d41015ecece8e5fdee11d2
vladoja <dr.jalo2@gmail.com>
2023-08-04 15:02:08 +0200

    Todo Model witch dummy data

---
+++ lib/data/dummy_data.dart
@@ -0,0 +1,7 @@
+import 'package:riverpod_simple_todo/models/todo.dart';
+
+const dummyTodoList = [
+  Todo("1", "Wash socks", false),
+  Todo("2", "Buy blueberries", false),
+  Todo("3", "Dispose trash", false),
+];
---
+++ lib/models/todo.dart
@@ -0,0 +1,7 @@
+class Todo {
+  const Todo(this.id, this.title, this.isDone);
+
+  final String id;
+  final String title;
+  final bool isDone;
+}
