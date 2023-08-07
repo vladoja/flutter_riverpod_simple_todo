bc65f88ce44e10bf651db4b32826dbdcf46ef60a
vladoja <dr.jalo2@gmail.com>
2023-08-07 18:34:24 +0200

    todosProvider refactored to StateNofitierProvider

--- lib/providers/todo_provider.dart
+++ lib/providers/todo_provider.dart
@@ -1,6 +1,21 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_simple_todo/data/dummy_data.dart';
+import 'package:riverpod_simple_todo/models/todo.dart';
 
-final todosProvider = Provider(
-  (ref) => dummyTodoList,
+class TodosNotifier extends StateNotifier<List<Todo>> {
+  TodosNotifier() : super(dummyTodoList);
+
+  void addTodo(Todo todo) {
+    Todo? alreadyExists = state.cast<Todo?>().firstWhere(
+          (element) => element!.id == todo.id,
+          orElse: () => null,
+        );
+    if (alreadyExists == null) {
+      state = [...state, todo];
+    }
+  }
+}
+
+final todosProvider = StateNotifierProvider<TodosNotifier, List<Todo>>(
+  (ref) => TodosNotifier(),
 );
